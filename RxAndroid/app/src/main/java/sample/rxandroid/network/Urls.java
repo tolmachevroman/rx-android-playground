fc59dd3357c8aab36c84515002d5f0c69b379440
tolmachevroman <tolmachevroman@gmail.com>
2015-09-25 11:55:40 -0300

    Urls class added

---
+++ RxAndroid/app/src/main/java/sample/rxandroid/network/Urls.java
@@ -0,0 +1,9 @@
+package sample.rxandroid.network;
+
+/**
+ * Created by romantolmachev on 25/9/15.
+ */
+public class Urls {
+
+    public static final String US_JOBS_SEARCH = "https://api.usa.gov/jobs/search.json";
+}
