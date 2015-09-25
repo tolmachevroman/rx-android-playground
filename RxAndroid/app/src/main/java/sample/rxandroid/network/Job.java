81e142a2824c6745a15300b3aa27478c910d5f04
tolmachevroman <tolmachevroman@gmail.com>
2015-09-25 11:53:47 -0300

    Job entity to use in open USA gov API

--- RxAndroid/app/src/main/java/sample/rxandroid/network/Job.java
+++ RxAndroid/app/src/main/java/sample/rxandroid/network/Job.java
@@ -2,12 +2,81 @@ package sample.rxandroid.network;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by romantolmachev on 25/9/15.
  */
 public class Job {
 
-    @SerializedName("")
+    @SerializedName("id")
     int id;
 
+    @SerializedName("position_title")
+    String positionTitle;
+
+    @SerializedName("organization_name")
+    String organizationName;
+
+    @SerializedName("rate_interval_code")
+    String rateIntervalCode;
+
+    @SerializedName("minimum")
+    int minimum;
+
+    @SerializedName("maximum")
+    int maximum;
+
+    @SerializedName("start_date")
+    Date startDate;
+
+    @SerializedName("end_date")
+    Date endDate;
+
+    @SerializedName("locations")
+    List<String> locations;
+
+    @SerializedName("url")
+    String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public String getRateIntervalCode() {
+        return rateIntervalCode;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public List<String> getLocations() {
+        return locations;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
