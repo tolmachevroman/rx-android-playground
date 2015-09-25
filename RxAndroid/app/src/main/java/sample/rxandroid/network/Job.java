4dea6963f9e37f7ab33a5beb7d86ef45a7c0b1df
tolmachevroman <tolmachevroman@gmail.com>
2015-09-25 11:49:28 -0300

    Missing Retrofit dependencies

---
+++ RxAndroid/app/src/main/java/sample/rxandroid/network/Job.java
@@ -0,0 +1,13 @@
+package sample.rxandroid.network;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by romantolmachev on 25/9/15.
+ */
+public class Job {
+
+    @SerializedName("")
+    int id;
+
+}
